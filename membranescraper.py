cf2954cf3511eb70590f113ee25fc220ac1dec52
shane-et-al <ssl2ab@virginia.edu>
2023-08-15 13:18:19 -0400

    improve date parsing

--- membranescraper.py
+++ membranescraper.py
@@ -12,17 +12,20 @@ def isMembraneLine(line, membranes):
 def isDateLine(line, dates):
     if line.strip().upper() == "NO DATE":
         return True
-    try:
-        parser.parse(line)
-        return True
-    except ValueError:
-        return False
+    if line in dates:
+        try:
+            if parser.parse(line).year<2001 and parser.parse(line).year>500:
+                return True
+        except ValueError:
+            return False
     return False
 
 # Parse the line and return it as a python datetime.date
 def parseDate(line):
     if line.strip().upper() == "NO DATE":
         return "No date"
+    if parser.parse(line).year < 1200 or parser.parse(line).year > 1500:
+        print(parser.parse(line).strftime("%Y/%m/%d"))
     return parser.parse(line).strftime("%Y/%m/%d")
 
 # Open the list of URLs
@@ -32,8 +35,8 @@ with open("urls.txt") as urls:
         html  = requests.get(url.strip()).text
         soup = bs4.BeautifulSoup(html, 'html.parser')
         t = soup.find_all("div",class_="inner")[0].text
-        # with open ("text.txt", "w") as out:
-        #     out.write(t)
+        with open ("text.txt", "w") as out:
+            out.write(t)
         h3s = soup.find_all("div",class_="inner")[0].find_all("h3")
         membranes = []
         # Grab all the header3s inside, which are membrane labels
@@ -44,20 +47,21 @@ with open("urls.txt") as urls:
         # Grab all the em tags, which are dates
         for em in ems:
             dates.append(em.text)
-        roll = soup.find_all("h1",class_="page-header")[0].text
+        roll = soup.find_all("h1",class_="page-header")[0].text.strip()
         membrane = None
         date = None
         text = ""
-
+        #First line is empty, so start from line 1
         for line in t.splitlines()[1:]:
             # Is the line not a date or a membrane (i.e. is text)
             if not isDateLine(line, dates) and not isMembraneLine(line,membranes):
                 text = text+" "+line.strip()
+                print(line.strip())
                 continue
             else:
                 # append entry only if it's complete (has date, membrane, text)
                 if date != None and membrane != None and len(text.strip())>0:
-                    entries.append({"roll":roll, "membrane":membrane, "date":date, "text":text})
+                    entries.append({"roll":roll, "membrane":membrane, "date":date, "text":text.strip()})
                 text = ""
                 if isDateLine(line, dates):
                     date = parseDate(line)
