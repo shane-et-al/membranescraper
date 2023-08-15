e9959d8c19fdbab4f5db4c744a0100a64f430bdb
shane-et-al <ssl2ab@virginia.edu>
2023-08-15 11:55:19 -0400

    fix date error

--- membranescraper.py
+++ membranescraper.py
@@ -1,30 +1,29 @@
 import requests
 import bs4
-import parsedatetime
+from dateutil import parser
 import datetime
 import csv
-
-cal = parsedatetime.Calendar()
 
 # Is the line a membrane header?
 def isMembraneLine(line, membranes):
     return line in membranes
 
 # Is the line a date?
-def isDateLine(line):
+def isDateLine(line, dates):
     if line.strip().upper() == "NO DATE":
         return True
-    date = datetime.date(*cal.parse(line)[0][:-6])
-    # parsedatetime hungrily parses nondate strings as the current datetime
-    # return False (not a date) if the year is in this millenium
-    return date.year<2001
+    try:
+        parser.parse(line)
+        return True
+    except ValueError:
+        return False
+    return False
 
 # Parse the line and return it as a python datetime.date
 def parseDate(line):
     if line.strip().upper() == "NO DATE":
         return "No date"
-    date = datetime.date(*cal.parse(line)[0][:-6])
-    return date
+    return parser.parse(line).strftime("%Y/%m/%d")
 
 # Open the list of URLs
 with open("urls.txt") as urls:
@@ -33,11 +32,18 @@ with open("urls.txt") as urls:
         html  = requests.get(url.strip()).text
         soup = bs4.BeautifulSoup(html, 'html.parser')
         t = soup.find_all("div",class_="inner")[0].text
+        # with open ("text.txt", "w") as out:
+        #     out.write(t)
         h3s = soup.find_all("div",class_="inner")[0].find_all("h3")
         membranes = []
-        # Grab all the headers inside, which are membrane labels
+        # Grab all the header3s inside, which are membrane labels
         for h3 in h3s:
             membranes.append(h3.text)
+        ems = soup.find_all("div",class_="inner")[0].find_all("em")
+        dates = []
+        # Grab all the em tags, which are dates
+        for em in ems:
+            dates.append(em.text)
         roll = soup.find_all("h1",class_="page-header")[0].text
         membrane = None
         date = None
@@ -45,20 +51,19 @@ with open("urls.txt") as urls:
 
         for line in t.splitlines()[1:]:
             # Is the line not a date or a membrane (i.e. is text)
-            if not isDateLine(line) and not isMembraneLine(line,membranes):
-                text = text+" "+line
+            if not isDateLine(line, dates) and not isMembraneLine(line,membranes):
+                text = text+" "+line.strip()
                 continue
             else:
                 # append entry only if it's complete (has date, membrane, text)
                 if date != None and membrane != None and len(text.strip())>0:
                     entries.append({"roll":roll, "membrane":membrane, "date":date, "text":text})
                 text = ""
-                if isDateLine(line):
+                if isDateLine(line, dates):
                     date = parseDate(line)
                 elif isMembraneLine(line, membranes):
                     membrane = line.strip()
-        entries.append({"roll":roll, "membrane":membrane, "date":date, "text":text})
-        break
+        entries.append({"roll":roll, "membrane":membrane, "date":date, "text":text.strip()})
 
     with open('rolls.csv', 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=entries[0].keys())
